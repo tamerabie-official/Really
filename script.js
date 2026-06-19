a70d1c42e60e62b950974d9a9e7e6bd83ba12773
Tamer Mohamed Rabie <mr.rabieeng@gmail.com>
2026-06-19 11:17:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -194,3 +194,14 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 }); // نهاية DOMContentLoaded
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+}
